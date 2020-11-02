b466e19e6f4e0637208218435ad887b997b57d4d
Ameer AB <ameerasyrafab@gmail.com>
2020-11-02 20:02:45 +0800

    Implemented an obstacle to move manually. Oscillator.

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Move platforms
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+
+    [Range(0,1)][SerializeField] float movementFactor; //0 for not moved, 1 for fully moved 1.9*10 = 19 Y is the top
+
+    private Vector3 startingPos;
+
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
